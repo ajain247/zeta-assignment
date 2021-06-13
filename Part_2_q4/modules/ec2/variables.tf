056ea0d167d3d9717a3cc46e5ea0339010917f14
Adarsh Jain <adarshjain247@gmail.com>
2021-06-13 20:02:11 +0530

    added config files for ec2

--- Part_2_q4/modules/ec2/variables.tf
+++ Part_2_q4/modules/ec2/variables.tf
@@ -1,16 +1,33 @@
-variable "ami_id"{
+variable "key_name"{
     type=string
 }
 
-variable "instance_type" {
-    type=string
-    default = "t2.micro"
-}
-
-variable "subnet_id"{
+variable "key_path"{
     type=string
 }
 
-variable "associate_public_ip_address"{
+variable "webserver_ami_id"{
     type=string
+}
+
+variable "appserver_ami_id"{
+    type=string
+}
+
+variable "appserver_instance_type" {
+    type=string
+}
+
+variable "webserver_instance_type" {
+    type=string
+}
+
+variable "public_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for the public subnets"
+}
+
+variable "private_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for the private subnets"
 }
