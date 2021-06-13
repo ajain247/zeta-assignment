d0ecbdf7af97b92fce721d7a34cb42f3684151a6
ajain247 <48178939+ajain247@users.noreply.github.com>
2021-06-13 23:18:28 +0530

    Update main.tf

--- Part_2_q4/modules/zeta_assignment_module/main.tf
+++ Part_2_q4/modules/zeta_assignment_module/main.tf
@@ -96,17 +96,51 @@ resource "aws_route_table_association" "private_rt_association" {
   route_table_id = aws_route_table.private_rt.id
 }
 
-resource "aws_security_group" "default_sg" {
+resource "aws_security_group" "webserver_sg" {
   vpc_id      = aws_vpc.vpc.id
-  name        = join("-", [var.name, "default-sg"])
-  description = "Default security group to allow traffic from VPC"
+  name        = join("-", [var.name, "webserver-sg"])
+  description = "Security group to allow traffic to webservers"
   ingress {
-    from_port = "0"
-    to_port   = "0"
-    protocol  = "-1"
-    self      = true
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "appserver_sg" {
+  vpc_id      = aws_vpc.vpc.id
+  name        = join("-", [var.name, "appserver-sg"])
+  description = "Security group to allow traffic to appservers"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [element(aws_subnet.public_subnet.*.id)]
+  }
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [element(aws_subnet.public_subnet.*.id)]
+  }
+  
   egress {
     from_port = "0"
     to_port   = "0"
